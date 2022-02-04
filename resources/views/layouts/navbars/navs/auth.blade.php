1af7f317d322527c1a6eea0758d7c8c2610c8673
blkopsdev <blkopsdev@gmail.com>
2022-02-03 16:53:14 -0800

    modified:   resources/views/layouts/navbars/navs/auth.blade.php

--- resources/views/layouts/navbars/navs/auth.blade.php
+++ resources/views/layouts/navbars/navs/auth.blade.php
@@ -47,7 +47,7 @@
           </a>
           <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownProfile">
             <a class="dropdown-item" href="{{ route('profile.edit') }}">{{ __('Profile') }}</a>
-            <a class="dropdown-item" href="#">{{ __('Settings') }}</a>
+            {{-- <a class="dropdown-item" href="#">{{ __('Settings') }}</a> --}}
             <div class="dropdown-divider"></div>
             <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">{{ __('app.logout') }}</a>
           </div>
