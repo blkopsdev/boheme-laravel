dd093c6960f2ba43086f116bb6d39b75e09f8875
blkopsdev <blkopsdev@gmail.com>
2022-02-10 14:19:07 -0800

    modified:   resources/views/customer/show.blade.php

--- resources/views/customer/show.blade.php
+++ resources/views/customer/show.blade.php
@@ -15,9 +15,12 @@
         </div>
       </div>
       <div class="row">
-        <div class="col-md-12">
-          <h2 class="d-flex align-items-center">{{ __('ID: ') . $customer->id }} | {{ $customer->first_name }} {{ $customer->last_name }} 
-            <a href="{{ route('customers.edit', $customer->id) }}" class="btn btn-primary btn-rounded ml-3" rel="tooltip" data-original-title="" title="{{ __('Edit') }}"><i class="material-icons mr-2">edit</i>{{ __('Edit') }}</a>
+        <div class="col-md-6">
+          <h2>{{ __('ID: ') . $customer->id }} | {{ $customer->first_name }} {{ $customer->last_name }} 
+          </h2>
+        </div>
+        <div class="col-md-6 d-flex align-items-center justify-content-end">
+          <a href="{{ route('customers.edit', $customer->id) }}" class="btn btn-primary btn-rounded ml-3" rel="tooltip" data-original-title="" title="{{ __('Edit') }}"><i class="material-icons mr-2">edit</i>{{ __('Edit') }}</a>
             @if (auth()->user()->user_type == "admin")
             <a href="{{ route('merge', $customer->id) }}" class="btn btn-success btn-rounded ml-3" rel="tooltip" data-original-title="" title="{{ __('Merge') }}"><i class="material-icons mr-2">merge</i>{{ __('Merge') }}</a>
             <form action="{{ route('customers.destroy',$customer->id) }}" method="POST" class="mb-1">
@@ -28,7 +31,6 @@
                 Delete</button>
             </form>
             @endif
-          </h2>
         </div>
       </div>
       <div class="row">
