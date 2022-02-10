b4fae7908b34c449b44aad566f9914d34163f494
blkopsdev <blkopsdev@gmail.com>
2022-02-10 13:06:24 -0800

    modified:   resources/views/layouts/footers/guest.blade.php

--- resources/views/layouts/footers/guest.blade.php
+++ resources/views/layouts/footers/guest.blade.php
@@ -1,19 +1,5 @@
 <footer class="footer">
     <div class="container">
-        <nav class="float-left">
-        <ul>
-            <li>
-            <a href="#">
-                {{ __('About Us') }}
-            </a>
-            </li>
-            <li>
-            <a href="#">
-                {{ __('Blog') }}
-            </a>
-            </li>
-        </ul>
-        </nav>
         <div class="copyright float-right">
         &copy;
         <script>
