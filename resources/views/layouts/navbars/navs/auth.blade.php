61d2f96859664c8666e12fd8f471af0bfd2626d8
blkopsdev <blkopsdev@gmail.com>
2022-02-10 11:54:16 -0800

    added the logo

--- resources/views/layouts/navbars/navs/auth.blade.php
+++ resources/views/layouts/navbars/navs/auth.blade.php
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-expand-lg navbar-fixed fixed-top ">
   <div class="container-fluid">
     <div class="col-md-3">
-      <h4 class="m-2">Boheme Transactions Ledger</h4>
+      <a href="/"><img src="{{ asset('assets/img/Logo.png') }}" alt="" height="50"></a>
     </div>
     <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
       <span class="sr-only">Toggle navigation</span>
--- resources/views/layouts/navbars/navs/guest.blade.php
+++ resources/views/layouts/navbars/navs/guest.blade.php
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top text-white">
   <div class="container">
     <div class="navbar-wrapper">
-      <h3>Boheme Transactions Ledger</h3>
+      <a href="/"><img src="{{ asset('assets/img/Logo.png') }}" alt="" height="65"></a>
     </div>
     <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
       <span class="sr-only">Toggle navigation</span>
