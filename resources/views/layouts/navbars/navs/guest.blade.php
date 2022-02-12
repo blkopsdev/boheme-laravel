cf80125237a5a38f505198716745a7f774aace78
blkopsdev <blkopsdev@gmail.com>
2022-02-11 16:48:30 -0800

    update logo hover color

--- resources/views/layouts/navbars/navs/guest.blade.php
+++ resources/views/layouts/navbars/navs/guest.blade.php
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top text-white">
   <div class="container">
     <div class="navbar-wrapper">
-      <a href="/" class="h3 text-warning logo"><img src="{{ asset('assets/img/Logo.png') }}" alt="" height="50" class="mr-3"> Trade-In Database </a>
+      <a href="/" class="h3 text-warning" id="logo"><img src="{{ asset('assets/img/Logo.png') }}" alt="" height="50" class="mr-3"> Trade-In Database </a>
     </div>
     <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
       <span class="sr-only">Toggle navigation</span>
