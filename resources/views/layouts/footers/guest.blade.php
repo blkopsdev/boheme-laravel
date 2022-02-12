f78c41f96f53e8b058db9a3908a0591d590dab74
blkopsdev <blkopsdev@gmail.com>
2022-02-12 14:04:17 -0800

    modified:   resources/views/layouts/footers/guest.blade.php

--- resources/views/layouts/footers/guest.blade.php
+++ resources/views/layouts/footers/guest.blade.php
@@ -1,10 +1,6 @@
 <footer class="footer">
     <div class="container">
         <div class="copyright float-right">
-        &copy;
-        <script>
-            document.write(new Date().getFullYear())
-        </script>, Powered by blkopsdev
         </div>
     </div>
 </footer>
